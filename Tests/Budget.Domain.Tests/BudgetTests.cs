4f152ef8b57b94e45aac7ac2ff3fc58c005766d3
bjjumba <ericjjumba5@gmail.com>
2025-01-31 10:20:16 +0300

    Fix tests

--- Tests/Budget.Domain.Tests/BudgetTests.cs
+++ Tests/Budget.Domain.Tests/BudgetTests.cs
@@ -7,7 +7,7 @@ public class BudgetTests
     [Fact]
     public void Should_Create_Budget()
     {
-        var budget = new BudgetEntity();
+        var budget = new BudgetEntity("Tests",13);
         
         Assert.Single(budget.GetDomainEvents());
     }
